15565257f2fa5c811dfeef216e9aa7848d3f0df7
Yve5 <yves.rouille@gmail.com>
2020-07-25 00:50:14 +0200

    feat. first commit

---
+++ precache-manifest.5a4975ed4dd8f8a866f2c2bf678dd09c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc956ea19c8c6cb9c4a8fe29943b8d99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f275a0c51a62477d427",
+    "url": "/static/css/main.2e73c415.chunk.css"
+  },
+  {
+    "revision": "41a3e780c0eba9bb4d20",
+    "url": "/static/js/2.00845a02.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.00845a02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f275a0c51a62477d427",
+    "url": "/static/js/main.d88e571c.chunk.js"
+  },
+  {
+    "revision": "290ea107094f1a5ba281",
+    "url": "/static/js/runtime-main.f455d250.js"
+  }
+]);
